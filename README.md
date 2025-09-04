33b96378c875e0cc7503d3bb649459cf56956ff1
Raydberg Gabriel <raydbergg@gmail.com>
2025-09-04 10:51:41 -0500

    Refactor GatewayConfig routes and update README with microservice API calls

---
+++ README.md
@@ -0,0 +1,7 @@
+# FitDesk Gateway
+
+# Llamadas a los microservicios
+
+```http
+http://localhost:9090/api/members/saludo
+```
--- src/main/java/com/gateway/beans/GatewayConfig.java
+++ src/main/java/com/gateway/beans/GatewayConfig.java
@@ -10,8 +10,30 @@ public class GatewayConfig {
     @Bean
     public RouteLocator routerLocator(RouteLocatorBuilder builder) {
         return builder.routes()
-                // .route(route -> route.path("/msvc-billing/**").uri("lb://msvc-billing"))
-                // .route(route -> route.path("/msvc-").uri(""))
+                .route("msvc-billing-route", route -> route
+                        .path("/billing/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://msvc-billing"))
+                .route("msvc-chat", route -> route
+                        .path("/chat/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://msvc-chat"))
+                .route("msvc-classes", route -> route
+                        .path("/classes/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://msvc-classes"))
+                .route("msvc-members", route -> route
+                        .path("/members/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://msvc-members"))
+                .route("msvc-notifications", route -> route
+                        .path("/notifications/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://msvc-notifications"))
+                .route("msvc-security", route -> route
+                        .path("/security/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("lb://msvc-security"))
                 .build();
     }
 }
