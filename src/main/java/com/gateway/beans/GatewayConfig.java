295c0a82bb860002b9504392c706f3e0d1a4ad38
Raydberg Gabriel <raydbergg@gmail.com>
2025-09-04 11:59:22 -0500

    Add AzureGatewayConfig for production routes and update GatewayConfig for non-prod

--- src/main/java/com/gateway/beans/GatewayConfig.java
+++ src/main/java/com/gateway/beans/GatewayConfig.java
@@ -4,8 +4,10 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!prod")
 public class GatewayConfig {
     @Bean
     public RouteLocator routerLocator(RouteLocatorBuilder builder) {
@@ -34,6 +36,40 @@ public class GatewayConfig {
                         .path("/security/**")
                         .filters(f -> f.stripPrefix(1))
                         .uri("lb://msvc-security"))
+                .build();
+    }
+}
+
+@Configuration
+@Profile("prod")
+class AzureGatewayConfig {
+    @Bean
+    public RouteLocator routerLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("msvc-billing-route", route -> route
+                        .path("/billing/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("http://msvc-billing:9093"))
+                .route("msvc-chat", route -> route
+                        .path("/chat/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("http://msvc-chat:9096"))
+                .route("msvc-classes", route -> route
+                        .path("/classes/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("http://msvc-classes:9094"))
+                .route("msvc-members", route -> route
+                        .path("/members/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("http://msvc-members:9092"))
+                .route("msvc-notifications", route -> route
+                        .path("/notifications/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("http://msvc-notifications:9095"))
+                .route("msvc-security", route -> route
+                        .path("/security/**")
+                        .filters(f -> f.stripPrefix(1))
+                        .uri("http://msvc-security:9091"))
                 .build();
     }
 }
