d595eb47076fd77c113623cd2a1d4afc1f96ee85
Raydberg Gabriel <raydbergg@gmail.com>
2025-10-08 00:05:07 -0500

    Simplificar la anotación @RequestMapping en FallbackController para aceptar solo el método GET.

--- src/main/java/com/gateway/fallback/FallbackController.java
+++ src/main/java/com/gateway/fallback/FallbackController.java
@@ -8,10 +8,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/fallback")
 public class FallbackController {
 
-    @RequestMapping(
-            value = "/{service}",
-            method = {RequestMethod.GET, RequestMethod.POST}
-    )
+    @RequestMapping(value = "/{service}")
     public ResponseEntity<String> genericServiceFallback(@PathVariable String service) {
         String body = String.format("El servicio '%s' no está disponible en este momento. Por favor, inténtalo más tarde.", service);
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(body);
