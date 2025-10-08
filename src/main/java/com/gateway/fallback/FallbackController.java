c40fd173d95000433630bcb552590eb0f58968e9
Raydberg Gabriel <raydbergg@gmail.com>
2025-10-07 23:26:54 -0500

    update

--- src/main/java/com/gateway/fallback/FallbackController.java
+++ src/main/java/com/gateway/fallback/FallbackController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/fallback")
 public class FallbackController {
 
-    @RequestMapping(value = "/{service}")
+    @RequestMapping(
+            value = "/{service}",
+            method = {RequestMethod.GET, RequestMethod.POST}
+    )
     public ResponseEntity<String> genericServiceFallback(@PathVariable String service) {
         String body = String.format("El servicio '%s' no está disponible en este momento. Por favor, inténtalo más tarde.", service);
         return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(body);
